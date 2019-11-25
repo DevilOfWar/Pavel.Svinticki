eef93b572350ef330649df2e1b4a1927b5004d63
Pavel Svinticki <57325225+DevilOfWar@users.noreply.github.com>
2019-11-25 08:34:35 +0300

    Add files via upload
    Created Vector class

--- EpamTask02dll/EpamTask02dll/Class1.cs
+++ EpamTask02dll/EpamTask02dll/Class1.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace EpamTask02dll
 {
-    public class Class1
+    public class Vector
     {
+        private double x, y, z;
+        public double GetX
+        {
+            get
+            {
+                return this.x;
+            }
+        }
+        public double GetY
+        {
+            get
+            {
+                return this.y;
+            }
+        }
+        public double GetZ
+        {
+            get
+            {
+                return this.z;
+            }
+        }
+        public void SetCoords(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public Vector()
+        {
+            this.x = 0;
+            this.y = 0;
+            this.z = 0;
+        }
+        public Vector(double x, double y, double z) : this()
+        {
+            this.SetCoords(x, y, z);
+        }
     }
 }
